5bb10de8d19ffa84080569ad5a7c49d0d5fb648c
Octoturge <iwo.strzebonski@gmail.com>
2022-12-02 18:07:42 +0100

    Init

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/iwostrzebonski/git/nuxt3-boilerplate/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/iwostrzebonski/git/nuxt3-boilerplate/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
